e8a78cc3e28506bc255fa12ee05018bff45509fa
a2jimenez <45322834+a2jimenez@users.noreply.github.com>
2020-02-27 16:31:26 -0600

    test
    hola

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import SEO from "../components/seo"
 import Layout from "../components/layout"
 
 const AboutUs = () => {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
-import SEO from "../components/seo"
 import Layout from "../components/layout"
 
 const BlogPage = () => {
